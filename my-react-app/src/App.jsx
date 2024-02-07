06288c336642c97e365a571888fab8a2f3351016
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-02-06 19:13:01 -0800

    Added Home component and removed excess CSS from initial react project

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
-      <section id='home'>HOME</section>
+      <Home/>
       <section id='about'>ABOUT</section>
       <section id='portfolio'>PORTFOLIO</section>
       <section id='resume'>RESUME</section>
---
+++ my-react-app/src/components/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div id="home" className="test-mode">
+            <p>I'm Oscar Lopez</p>
+            <p>Description</p>
+            <p>Social Media Buttons</p>
+            <p>Navigation Arrow</p>
+        </div>
+    );
+}
