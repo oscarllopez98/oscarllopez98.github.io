b75cf08776e40da7153a9f85bf239b8105fa3d1f
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-02-06 18:57:44 -0800

    Add components folder and removed unnecessary content from App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id='home'></section>
-      <section id='about'></section>
-      <section id='portfolio'></section>
-      <section id='resume'></section>
-      <section id='hobbies-and-interests'></section>
+      <section id='home'>HOME</section>
+      <section id='about'>ABOUT</section>
+      <section id='portfolio'>PORTFOLIO</section>
+      <section id='resume'>RESUME</section>
+      <section id='hobbies-and-interests'>HOBBIES AND INTERESTS</section>
     </>
   )
 }
