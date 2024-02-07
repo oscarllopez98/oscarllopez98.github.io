aff042075d8c6bf437b9011ab6232b3ff227d037
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-02-06 21:18:08 -0800

    Added Navbar component and content. Added CSS for Navbar component and items

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav id="navbar" className="test-mode-blue">
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">ABOUT</a></li>
+                <li><a href="">PORTFOLIO</a></li>
+                <li><a href="">RESUME</a></li>
+                <li><a href="">HOBBIES & INTERESTS</a></li>
+            </ul>
+        </nav>
+    );
+}
