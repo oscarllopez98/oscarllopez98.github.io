dc5804f5c4acdb58eaea3fd63ab28aa01bd59886
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-02-06 22:10:51 -0800

    Moved Navbar component to within Home component

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -1,8 +1,10 @@
 import SocialMediaButtons from "./SocialMediaButtons";
+import Navbar from "./Navbar";
 
 export default function Home() {
     return (
         <div id="home" className="test-mode">
+            <Navbar/>
             <h1>I'm Oscar Lopez.</h1>
             <p>Description</p>
             <SocialMediaButtons/>
