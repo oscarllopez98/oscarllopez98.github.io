8f0369d54c9d7b33d4d875b66a53baee29298df6
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-02-07 15:07:12 -0800

    Added old headerbackground (can be changed). Added another jpg that I might use as the background image. Added JS to Home component to get height and width of screen (not quite right yet). Added CSS to adjust background image.

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -1,14 +1,42 @@
 import SocialMediaButtons from "./SocialMediaButtons";
 import Navbar from "./Navbar";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+
+    // Update width and height state when the window is resized
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.width);
+            setHeight(window.height);
+        };
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Clean up event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+    
+
+    const description = `I\'m not just an experienced Quality Assurance Engineer ;
+     I\'m a future-minded Software Engineer driven by an unyielding passion for innovation, 
+     relentless critical thinking, and a knack for unraveling complex problems. 
+     Start scrolling and discover more about me!`
+
     return (
-        <div id="home" className="test-mode">
-            <Navbar/>
+        <header id="home" className="test-mode" style={{ width: width, height: height }}>
+            <Navbar />
             <h1>I'm Oscar Lopez.</h1>
-            <p>Description</p>
-            <SocialMediaButtons/>
+            <article>{description}</article>
+            <SocialMediaButtons />
             <p>Navigation Arrow</p>
-        </div>
+        </header>
     );
 }
