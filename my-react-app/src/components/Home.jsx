d0aa30b4c77f7bdc8ec7cc215f9e6a03069153a9
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-02-07 22:45:05 -0800

    Added new Google Font called Roboto. Added CSS for decorating home text. Added CSS fonts folder to hold the implementation of each font-family

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -23,18 +23,17 @@ export default function Home() {
     }, []);
 
 
-    const description = `I\'m not just an experienced Quality Assurance Engineer ;
-     I\'m a future-minded Software Engineer driven by an unyielding passion for innovation, 
-     relentless critical thinking, and a knack for unraveling complex problems. 
-     Start scrolling and discover more about me!`
+    const description = `I\'m not just an <span>experienced Quality Assurance Engineer</span> ;
+     I\'m a <span>future-minded Software Engineer</span> driven by an unyielding passion for innovation, 
+     relentless critical thinking, and a knack for unraveling complex problems.<br/> 
+     <span>Start scrolling</span> and discover more <span>about me!</span>`
 
     return (
         <header id="home" className="test-mode" style={{ height: height }}>
             <Navbar />
-
             <div id="home-text">
                 <h1>I'm Oscar Lopez.</h1>
-                <article>{description}</article>
+                <article dangerouslySetInnerHTML={{ __html: description}}></article>
                 <SocialMediaButtons />
             </div>
 
