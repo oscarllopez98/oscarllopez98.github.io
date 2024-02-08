a807539d6b1c1b3c74fb53a60f6ef96353200860
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-02-07 20:32:09 -0800

    Adjusted Home component to fix sizing issue. Added CSS to help center Home text

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -4,14 +4,13 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
 
-    const [width, setWidth] = useState(window.innerWidth);
+    // const [width, setWidth] = useState(window.innerWidth);
     const [height, setHeight] = useState(window.innerHeight);
 
     // Update width and height state when the window is resized
     useEffect(() => {
         const handleResize = () => {
-            setWidth(window.width);
-            setHeight(window.height);
+            setHeight(window.innerHeight);
         };
 
         // Add event listener for window resize
@@ -22,7 +21,7 @@ export default function Home() {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
-    
+
 
     const description = `I\'m not just an experienced Quality Assurance Engineer ;
      I\'m a future-minded Software Engineer driven by an unyielding passion for innovation, 
@@ -30,11 +29,15 @@ export default function Home() {
      Start scrolling and discover more about me!`
 
     return (
-        <header id="home" className="test-mode" style={{ width: width, height: height }}>
+        <header id="home" className="test-mode" style={{ height: height }}>
             <Navbar />
-            <h1>I'm Oscar Lopez.</h1>
-            <article>{description}</article>
-            <SocialMediaButtons />
+
+            <div id="home-text">
+                <h1>I'm Oscar Lopez.</h1>
+                <article>{description}</article>
+                <SocialMediaButtons />
+            </div>
+
             <p>Navigation Arrow</p>
         </header>
     );
