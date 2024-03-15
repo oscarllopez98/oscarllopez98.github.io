547beb4f19d918b9666f10dbe37a166a68efa981
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-03-15 12:55:32 -0700

    Fixed formatting for adding Badge to every screen size

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -17,17 +17,17 @@ export default function Certificate({ cert }) {
                 {
                     certBadgeSrc.length > 0 ?
                         <>
-                            <div className="col-12 col-sm-9 col-md-9 col-lg-9 col-xl-9 px-0 mx-0"> {/* Set padding and margin to 0 */}
+                            <div className="col-12 col-sm-8 col-md-8 col-lg-9 col-xl-8 px-0 mx-0"> 
                                 <h3>{certTitle}</h3>
                                 <p className="certificate-subtitle"><span id="certificate-subtitle" className="italics">{certSubtitle}<br className="subtitle-break" /> </span><span className="bullet-point"></span> {certDuration}</p>
                             </div>
-                            <div id="certificate-badge-col" className="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-2 px-0 mx-0"> {/* Set padding and margin to 0 */}
+                            <div id="certificate-badge-col" className="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-2 px-0 mx-0">
                                 <img className="certificate-badge" src={`${certBadgeSrc}`} alt={`Certificate badge for: ${certSubtitle}`} />
                             </div>
                         </>
                         : 
                         <>
-                        <div className="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-12 px-0 mx-0"> {/* Set padding and margin to 0 */}
+                        <div className="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-12 px-0 mx-0">
                             <h3>{certTitle}</h3>
                             <p className="certificate-subtitle"><span className="italics">{certSubtitle}<br className="subtitle-break" /> </span><span className="bullet-point"></span> {certDuration}</p>
                         </div>
