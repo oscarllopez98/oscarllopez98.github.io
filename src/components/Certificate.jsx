aebc1ea50fa98c95a2433e6a82d1aa5346e4ee35
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-03-15 10:57:56 -0700

    COmmented out old build files for testing locally. Added badges for certificates. Added placeholder area for where badges will go next to certificate title and subtitile

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -12,10 +12,21 @@ export default function Certificate({ cert }) {
 
     return (<>
         <div id={'certificate-' + certID} className="certificate col-12 col-sm-12 col-md-12 col-lg-8 col-xl-9">
-            <h3>{certTitle}</h3>
-            <p className="certificate-subtitle"><span className="italics">{certSubtitle}<br className="subtitle-break"/> </span><span className="bullet-point"></span> {certDuration}</p>
-            <p className="certificate-description">{certDescription}</p>
-            <div className="certificate-button"><DownloadButton src={certSrc} buttonType="certificate" downloadName={certTitle} title="Download Certificate" /></div>
+            <div className="row">
+                <div className="col-12 col-sm-9 col-md-9 col-lg-8 col-xl-8">
+                    <h3>{certTitle}</h3>
+                    <p className="certificate-subtitle"><span className="italics">{certSubtitle}<br className="subtitle-break" /> </span><span className="bullet-point"></span> {certDuration}</p>
+                </div>
+                <div className="col-12 col-sm-3 col-md-3 col-lg-4 col-xl-4">
+                    <p>Image placeholder</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <p className="certificate-description">{certDescription}</p>
+                    <div className="certificate-button"><DownloadButton src={certSrc} buttonType="certificate" downloadName={certTitle} title="Download Certificate" /></div>
+                </div>
+            </div>
         </div>
     </>);
 }
